e71fe377da855c90ae5575fd4da5621e01d5a240
KudzaiMunyukwa95 <kaymunyukwa@gmail.com>
2025-11-25 13:39:18 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -69,7 +69,7 @@ function App() {
             <Route
               path="/clients"
               element={
-                <PrivateRoute adminOnly>
+                <PrivateRoute>
                   <ClientManagement />
                 </PrivateRoute>
               }
