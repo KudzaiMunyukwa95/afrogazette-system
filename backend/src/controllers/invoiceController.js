3067caa9f02ce31ec373b4f787646bf46f873aac
KudzaiMunyukwa95 <kaymunyukwa@gmail.com>
2025-11-30 19:24:14 +0200

    Update invoiceController.js

--- backend/src/controllers/invoiceController.js
+++ backend/src/controllers/invoiceController.js
@@ -86,8 +86,8 @@ const generateInvoicePDF = async (invoiceData, filePath) => {
             headerY += 12;
             doc.text('support@afrogazette.co.zw | +263 77 8826661', marginX, headerY, { align: 'right', width: pageWidth - 2 * marginX });
 
-            // --- INVOICE TITLE & METADATA (16px spacing from header) ---
-            const contentTop = headerHeight + 24;
+            // --- INVOICE TITLE & METADATA (Increased spacing) ---
+            const contentTop = headerHeight + 50;
             doc.font('Helvetica-Bold').fontSize(28).fillColor(TEXT_DARK);
             doc.text('INVOICE', marginX, contentTop);
 
@@ -192,47 +192,12 @@ const generateInvoicePDF = async (invoiceData, filePath) => {
             doc.image(qrCodeDataUrl, marginX, footerY + 10, { width: 80, height: 80 });
 
             // Social Icons
-            const drawIcon = (pathStr, x, y, scale = 0.7) => {
-                try {
-                    if (isNaN(x) || isNaN(y)) return;
-                    doc.save();
-                    doc.translate(x, y);
-                    doc.scale(scale);
-                    doc.path(pathStr).fill(BRAND_BLACK);
-                    doc.restore();
-                } catch (err) {
-                    console.error('Icon error:', err);
-                }
-            };
-
-            const iconY = footerY + 18;
-            const colWidthSocial = 140;
-            const rowGap = 16; // Tighter row spacing
+            // Social Icons Removed - Text Only
             const socialX = marginX + 100;
-
             doc.font('Helvetica-Bold').fontSize(9).fillColor(TEXT_DARK);
             doc.text('Connect With Us', socialX, footerY);
-
-            // Column 1
-            drawIcon('M12.04 2c-5.46 0-9.91 4.45-9.91 9.91 0 1.75.46 3.45 1.32 4.95L2.05 22l5.25-1.38c1.45.79 3.08 1.21 4.74 1.21 5.46 0 9.91-4.45 9.91-9.91 0-2.65-1.03-5.14-2.9-7.01A9.816 9.816 0 0 0 12.04 2m.01 1.67c2.2 0 4.26.86 5.82 2.42a8.225 8.225 0 0 1 2.41 5.83c0 4.54-3.7 8.23-8.24 8.23-1.48 0-2.93-.39-4.19-1.15l-.3-.18-3.12.82.83-3.04-.19-.31a8.19 8.19 0 0 1-1.26-4.38c0-4.54 3.7-8.24 8.24-8.24M8.53 7.33c-.19-.43-.43-.43-.63-.44-.18 0-.38 0-.58 0-.2 0-.52.08-.79.38-.27.29-1.04 1.01-1.04 2.47s1.06 2.86 1.21 3.07c.15.21 2.09 3.2 5.06 4.49.71.31 1.26.49 1.69.63.72.23 1.37.19 1.88.11.58-.09 1.76-.72 2.01-1.42.25-.7.25-1.29.17-1.42-.08-.14-.28-.21-.58-.36-.3-.15-1.76-.87-2.03-1.01-.27-.13-.46-.19-.66.14-.2.32-.77.97-.94 1.17-.17.2-.34.23-.64.08-.3-.15-1.26-.46-2.4-1.47-.89-.79-1.49-1.77-1.66-2.07-.17-.3-.02-.46.13-.61.13-.13.29-.34.44-.51.15-.17.2-.29.3-.49.1-.19.05-.36-.02-.51-.08-.15-.7-1.69-.96-2.31Z', socialX, iconY);
-            doc.font('Helvetica').fontSize(8).text('+263 77 8826661', socialX + 20, iconY + 4);
-
-            drawIcon('M12 2.04c-5.5 0-10 4.49-10 10.02 0 5 3.66 9.15 8.44 9.9v-7H7.9v-2.9h2.54V9.85c0-2.51 1.49-3.89 3.78-3.89 1.09 0 2.23.19 2.23.19v2.47h-1.26c-1.24 0-1.63.77-1.63 1.56v1.88h2.78l-.45 2.9h-2.33v7a10 10 0 0 0 8.44-9.9c0-5.53-4.5-10.02-10-10.02Z', socialX, iconY + rowGap);
-            doc.text('AfroGazette News', socialX + 20, iconY + rowGap + 4);
-
-            drawIcon('M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8 1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5 5 5 0 0 1-5 5 5 5 0 0 1-5-5m0 2a3 3 0 0 0-3 3 3 3 0 0 0 3 3 3 3 0 0 0 3-3 3 3 0 0 0-3-3Z', socialX, iconY + rowGap * 2);
-            doc.text('AfroGazette News', socialX + 20, iconY + rowGap * 2 + 4);
-
-            // Column 2
-            const col2X = socialX + colWidthSocial;
-            drawIcon('M12.5 3a.5.5 0 0 0-.5.5v3.5a.5.5 0 0 0 .5.5 4 4 0 0 1 4 4 .5.5 0 0 0 .5.5h3.5a.5.5 0 0 0 .5-.5v-3.5a.5.5 0 0 0-.5-.5 7 7 0 0 0-7-7zM8 13a5 5 0 1 0 5 5V6a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v7.5A1.5 1.5 0 1 1 8 13z', col2X, iconY);
-            doc.text('AfroGazette News', col2X + 20, iconY + 4);
-
-            drawIcon('M21.58 7.19a2.7 2.7 0 0 0-1.9-1.9C18 5 12 5 12 5s-6 0-7.68.29a2.7 2.7 0 0 0-1.9 1.9C2 8.87 2 12.38 2 12.38s0 3.51.42 5.19a2.7 2.7 0 0 0 1.9 1.9C6 19.77 12 19.77 12 19.77s6 0 7.68-.29a2.7 2.7 0 0 0 1.9-1.9C22 15.89 22 12.38 22 12.38s0-3.51-.42-5.19zM10 15.5V9.27l6.25 3.12L10 15.5z', col2X, iconY + rowGap);
-            doc.text('AfroGazette News', col2X + 20, iconY + rowGap + 4);
-
-            drawIcon('M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8 8 0 0 1-8 8zm0-14a6 6 0 1 0 6 6 6 6 0 0 0-6-6zm0 10a4 4 0 1 1 4-4 4 4 0 0 1-4 4z', col2X, iconY + rowGap * 2);
-            doc.text('www.afrogazette.co.zw', col2X + 20, iconY + rowGap * 2 + 4);
+            doc.font('Helvetica').fontSize(8).text('www.afrogazette.co.zw', socialX, footerY + 15);
+            doc.text('+263 77 8826661', socialX, footerY + 30);
 
             doc.end();
 
