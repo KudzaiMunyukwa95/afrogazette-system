3fe41c216f5c3f6c55c96b346601b1191b9a912e
KudzaiMunyukwa95 <kaymunyukwa@gmail.com>
2025-12-01 10:21:31 +0200

    Update notificationRoutes.js

--- backend/src/routes/notificationRoutes.js
+++ backend/src/routes/notificationRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { verifyToken } = require('../middleware/authMiddleware');
+const { verifyToken } = require('../middleware/auth');
 const { getNotifications, markAsRead, markAllAsRead } = require('../controllers/notificationController');
 
 // All routes require authentication
